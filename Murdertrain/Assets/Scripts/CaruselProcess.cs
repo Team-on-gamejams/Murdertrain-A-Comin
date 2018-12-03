c0d4f9013c2e95e15e34cb033ba55dfc47623a32
Team <timonsol13@gmail.com>
2018-12-03 19:18:30 +0200

    Carusel

---
+++ Murdertrain/Assets/Scripts/CaruselProcess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaruselProcess : MonoBehaviour {
+
+	public void GivePoints(int points){
+		ScoreController.inst.AddToScore(points);
+	}
+}
